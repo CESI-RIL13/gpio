743287f5234a564eff8f562b55ad849db023f5b3
cedric moizeau <cedric@debian>
2015-02-04 15:47:41 +0100

    fin recieveMorse

--- cedric/recieveMorse.py
+++ cedric/recieveMorse.py
@@ -4,10 +4,11 @@ __author__ = 'cedric'
 import wiringpi2 as wpi
 import morse
 from datetime import datetime
-from time import sleep
+from time import sleep, time
 bouton=""
 debut=datetime.now()
 duree=""
+code=""
 PIN=0
 wpi.wiringPiSetup()
 
@@ -21,9 +22,29 @@ wpi.wiringPiSetup()
 
 while(1):
     bouton=wpi.digitalRead(PIN)
+    debut=datetime.now()
     if bouton == 1:
-        debut=datetime.now()
         while bouton == 1:
             bouton=wpi.digitalRead(PIN)
             sleep(0.02)
-        print datetime.now()-debut
+        duree=datetime.now()-debut
+        if duree.seconds < 0.5:
+            code += "."
+        else:
+            code += "-"
+    else:
+        while bouton == 0:
+            bouton=wpi.digitalRead(PIN)
+            sleep(0.02)
+        duree=datetime.now()-debut
+        if duree.seconds > 1:
+            code += " "
+        elif duree.seconds >2:
+            break
+
+code = code.split()
+retour = ""
+for lettre in code:
+    retour += morse.fromMorse(lettre)
+
+print retour
