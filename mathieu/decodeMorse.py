8667eddca167481310f6068c99c93e7a7145b857
mathieu <your@example.com>
2015-02-04 15:32:01 +0100

    modif mat

--- mathieu/decodeMorse.py
+++ mathieu/decodeMorse.py
@@ -1,43 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-import time
-from morse import *
+__author__ = 'cedric'
 import wiringpi2 as wpi
+import morse
+import time
+from time import sleep
+bouton=""
+debut=time.time()
+duree=""
+PIN=0
 wpi.wiringPiSetup()
 
-PIN = 0
-LED_SHORT = 0.25
-LED_LONG = 1
-MOT = 2
-CARACTERE = 0.5
-
-def clignoteLED(morseCar):
-    for car in morseCar:
-        if(car == '.'):
-            wpi.digitalWrite(PIN, 1)
-            time.sleep(LED_SHORT)
-        elif(car == '-'):
-            wpi.digitalWrite(PIN, 1)
-            time.sleep(LED_LONG)
-        else:
-            time.sleep(MOT)
-
-        wpi.digitalWrite(PIN, 0)
-        time.sleep(CARACTERE)
-
-
-def writeMorseCode():
-    car =''
-    if wpi.digitalRead(PIN):
-        print "je lis le pin %d" %PIN
+while(1):
+    bouton=wpi.digitalRead(PIN)
+    if bouton == 1:
+        debut=time.time()
+        while bouton == 1:
+            bouton=wpi.digitalRead(PIN)
+            sleep(0.02)
+        temps = time.time()-debut
+	if(temps < 1):
+		print "."
+	else :
+		print "_"
 	
-
-
-while 1:
-    try:
-        writeMorseCode()
-	time.sleep(0.1)
-    except KeyboardInterrupt:
-        print "salut!!"
-	break
