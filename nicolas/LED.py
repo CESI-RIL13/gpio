9422abe387ec8de70e743af345e709f852a51bfb
Nicolas Hermel <nhermel@edge-airport.com>
2015-02-04 14:34:57 +0100

    ajout de la fonction décode morse

--- nicolas/LED.py
+++ nicolas/LED.py
@@ -1,25 +1,45 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import time
 from morse import *
 import wiringpi2 as wpi
 wpi.wiringPiSetup()
 
+PIN = 7
+LED_SHORT = 0.25
+LED_LONG = 1
+MOT = 2
+CARACTERE = 0.5
+
 def clignoteLED(morseCar):
     for car in morseCar:
         if(car == '.'):
-            wpi.digitalWrite(7, 1)
-            time.sleep(0.25)
+            wpi.digitalWrite(PIN, 1)
+            time.sleep(LED_SHORT)
         elif(car == '-'):
-            wpi.digitalWrite(7, 1)
-            time.sleep(1)
+            wpi.digitalWrite(PIN, 1)
+            time.sleep(LED_LONG)
         else:
-            time.sleep(2)
+            time.sleep(MOT)
 
-        wpi.digitalWrite(7, 0)
-        time.sleep(0.5)
+        wpi.digitalWrite(PIN, 0)
+        time.sleep(CARACTERE)
 
-phrase = 'hello world'
 
-for car in phrase:
-    clignoteLED(toMorse(car.upper()))
+def writeMorseCode():
+    car =''
+    if wpi.digitalRead(PIN):
+        print "je lis le pin %d" %PIN
+
+
+# phrase = 'hello world'
+#
+# for car in phrase:
+#     clignoteLED(toMorse(car.upper()))
+
+while 1:
+    try:
+        writeMorseCode()
+    except KeyboardInterrupt:
+        print "salut!!"
